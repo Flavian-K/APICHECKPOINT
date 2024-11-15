c9570435550aff44e929a16787c1d2c88df731ed
Flavian-K <flaviankibe@gmail.com>
2024-11-15 22:34:35 +0300

    This React app fetches a list of users from the JSONPlaceholder API and displays their names and emails in a styled list.

---
+++ src/UserList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./UserList.css";
+
+const UserList = () => {
+	const [listOfUsers, setListOfUsers] = useState([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const response = await axios.get(
+					"https://jsonplaceholder.typicode.com/users"
+				);
+				setListOfUsers(response.data);
+			} catch (error) {
+				console.error("Error fetching users:", error);
+			}
+		};
+
+		fetchUsers();
+	}, []);
+
+	return (
+		<div>
+			<h1>User List</h1>
+			<ul>
+				{listOfUsers.map((user) => (
+					<li key={user.id}>
+						{user.name} - {user.email}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default UserList;
